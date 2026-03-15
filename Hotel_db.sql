6771df80f152cc76a770ce9b5751e9007692e61f
Tanushri014 <matretanushri0@gmail.com>
2026-03-15 11:01:48 +0530

    Add files via upload

---
+++ Hotel_db.sql
@@ -0,0 +1,18 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+create database hotel_db;
+
+use hotel_db;
+
+CREATE TABLE reservations (
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+    guest_name VARCHAR(100) NOT NULL,
+    room_number INT NOT NULL,
+    contact_number VARCHAR(15) NOT NULL,
+    reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+SHOW TABLES;
+
+DESC reservations;
+
+select * from reservations;
+
